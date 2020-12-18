2ced6ef29f757e7714c0616f37ce5394e8fe708a
Tom McClintock <tmcclintock89@gmail.com>
2020-12-17 20:26:47 -0800

    Made square and hex cell.

--- donjuan/cell.py
+++ donjuan/cell.py
@@ -1,31 +1,79 @@
+from abc import ABC
 from typing import Any, List, Optional
 
 from donjuan.doorspace import Doorspace
-from donjuan.face import Faces
+from donjuan.face import Faces, HexFaces, SquareFaces
 
 
-class Cell:
+class Cell(ABC):
     """
     The cell represents a single 'unit' in the map. The attributes on the cell
     define what exists in that unit. This includes the terrain, doors,
     walls, lights, other room details etc.
+    """
+
+    def __init__(
+        self,
+        faces: Faces,
+        filled: bool = False,
+        doorspace: Optional[Doorspace] = None,
+        contents: Optional[List[Any]] = None,
+    ):
+        self.faces = faces
+        self.filed = filled
+        self.doorspace = doorspace
+        self.contents = contents or []
+
+    @property
+    def n_sides(self) -> int:
+        return len(self.faces)
+
+
+class SquareCell(Cell):
+    """
+    A cell for a square grid.
 
     Args:
+      faces (Optional[SquareFaces]): faces of the cell
       filled (bool, optional): flag indicating whether the cell is
         filled (default ``False``)
       doorspace (Optional[Doorspace]): kind of doorway in this cell
-      faces (Optional[Faces]): faces of the cell
       contents (Optional[List[Any]]): things in this cell
     """
 
     def __init__(
         self,
+        faces: Optional[SquareFaces] = None,
         filled: bool = False,
         doorspace: Optional[Doorspace] = None,
-        faces: Optional[Faces] = None,
         contents: Optional[List[Any]] = None,
     ):
-        self.filled = filled
-        self.doorspace = doorspace
-        self.faces = faces or Faces()
-        self.contents = contents or []
+        faces = faces or SquareFaces()
+        super().__init__(
+            faces=faces, filled=filled, doorspace=doorspace, contents=contents
+        )
+
+
+class HexCell(Cell):
+    """
+    A cell for a hexagonal grid.
+
+    Args:
+      faces (Optional[HexFaces]): faces of the cell
+      filled (bool, optional): flag indicating whether the cell is
+        filled (default ``False``)
+      doorspace (Optional[Doorspace]): kind of doorway in this cell
+      contents (Optional[List[Any]]): things in this cell
+    """
+
+    def __init__(
+        self,
+        faces: Optional[HexFaces] = None,
+        filled: bool = False,
+        doorspace: Optional[Doorspace] = None,
+        contents: Optional[List[Any]] = None,
+    ):
+        faces = faces or HexFaces()
+        super().__init__(
+            faces=faces, filled=filled, doorspace=doorspace, contents=contents
+        )
--- tests/cell_test.py
+++ tests/cell_test.py
@@ -1,13 +1,23 @@
 from unittest import TestCase
 
-from donjuan import Cell
+from donjuan import HexCell, SquareCell
 
 
-class CellTest(TestCase):
+class SquareCellTest(TestCase):
     def test_smoke(self):
-        c = Cell()
+        c = SquareCell()
         assert c is not None
 
     def test_faces(self):
-        c = Cell()
+        c = SquareCell()
         assert len(c.faces) == 4
+
+
+class HexCellTest(TestCase):
+    def test_smoke(self):
+        c = HexCell()
+        assert c is not None
+
+    def test_faces(self):
+        c = HexCell()
+        assert len(c.faces) == 6
