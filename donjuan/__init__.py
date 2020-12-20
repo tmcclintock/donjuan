7ded8931ec04f9ce352d5aa6b83d4100c9889513
jammcc <jammcc27@gmail.com>
2020-12-20 15:07:33 -0500

    refactor doorspace --> door_space

--- donjuan/__init__.py
+++ donjuan/__init__.py
@@ -2,6 +2,6 @@ __version__ = "0.0.2"
 __docs__ = "Package for generating dungeons."
 
 from .cell import Cell, HexCell, SquareCell
-from .doorspace import Archway, Door, Doorspace, Portcullis
+from .door_space import Archway, Door, DoorSpace, Portcullis
 from .dungeon import Dungeon
 from .face import BareFace, Face, Faces, HexFaces, SquareFaces
