ec7d97df82810f88c0b7e3f3904e5e773e752697
Tom McClintock <tmcclintock89@gmail.com>
2020-12-20 11:14:17 -0800

    Added a long description.

--- setup.py
+++ setup.py
@@ -5,12 +5,21 @@ from setuptools import setup
 
 import donjuan  # noqa: E402
 
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="Donjuan",
     author="Tom McClintock",
     author_email="thmsmcclintock@gmail.com",
     version=donjuan.__version__,
     description=donjuan.__docs__,
+    long_description=long_description,
     url="https://github.com/tmcclintock/PyDonJuan",
     packages=["donjuan"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
