69466b134760b83f84f7525d9d4de7bfd6b5fea1
Tom McClintock <tmcclintock89@gmail.com>
2020-12-20 17:14:56 -0800

    Added a test of the render function.

--- tests/renderer_test.py
+++ tests/renderer_test.py
@@ -1,9 +1,17 @@
+import os
+import tempfile
 from unittest import TestCase
 
-from donjuan import Renderer
+import pytest
+
+from donjuan import Dungeon, Renderer
 
 
 class RendererTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.TEMP_DIR = tempfile.mkdtemp()
+
     def test_smoke(self):
         r = Renderer()
         assert r is not None
@@ -11,3 +19,11 @@ class RendererTest(TestCase):
     def test_scale(self):
         r = Renderer(scale=3)
         assert r.scale == 3
+
+    @pytest.mark.slow
+    def test_render(self):
+        fp = os.path.join(self.TEMP_DIR, "rendered_dungeon.png")
+        r = Renderer()
+        dungeon = Dungeon(n_rows=5, n_cols=5)
+        r.render(dungeon, file_path=fp)
+        assert os.path.exists(fp)
