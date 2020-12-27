b21616f1c4ed6edd9965619cf3470d571d4f8b4b
Tom McClintock <tmcclintock89@gmail.com>
2020-12-27 12:32:49 -0800

    Made n_cols and n_rows attributes of the Dungeon itself. Dungeon is now also composed of Randomizers.

--- donjuan/dungeon.py
+++ donjuan/dungeon.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from donjuan.grid import Grid, SquareGrid
 from donjuan.room import Room
@@ -11,9 +11,11 @@ class Dungeon:
         n_cols: Optional[int] = 5,
         grid: Optional[Grid] = None,
         rooms: Optional[Dict[str, Room]] = None,
+        randomizers: Optional[List["Randomizer"]] = None,
     ):
         self._grid = grid or SquareGrid(n_rows, n_cols)
         self._rooms = rooms or {}
+        self._randomizers = randomizers or []
 
     def add_room(self, room: Room) -> None:
         self._rooms[room.name] = room
@@ -23,5 +25,26 @@ class Dungeon:
         return self._grid
 
     @property
+    def n_cols(self) -> int:
+        return self.grid.n_cols
+
+    @property
+    def n_rows(self) -> int:
+        return self.grid.n_rows
+
+    @property
+    def randomizers(self) -> List["Randomizer"]:
+        return self._randomizers
+
+    @property
     def rooms(self) -> Dict[str, Room]:
         return self._rooms
+
+    def randomize(self) -> None:
+        """
+        For each item in :attr:`randomizers`, run the
+        :meth:`Randomizer.randomize_dungeon` method on this dungeon.
+        """
+        for rng in self.randomizers:
+            rng.randomize_dungeon(self)
+        return
--- tests/dungeon_test.py
+++ tests/dungeon_test.py
@@ -7,6 +7,8 @@ class DungeonTest(TestCase):
     def test_smoke(self):
         d = Dungeon()
         assert d is not None
+        assert d.grid is not None
+        assert d.randomizers == []
 
     def test_initial_attributes(self):
         d = Dungeon()
@@ -22,16 +24,16 @@ class DungeonTest(TestCase):
         hg = HexGrid(4, 5)
         d = Dungeon(grid=hg)
         assert isinstance(d.grid, HexGrid)
-        assert d.grid.n_rows == 4
-        assert d.grid.n_cols == 5
+        assert d.n_rows == 4
+        assert d.n_cols == 5
 
     def test_pass_dimensions(self):
         d = Dungeon(n_rows=4, n_cols=5)
-        assert d.grid.n_rows == 4
-        assert d.grid.n_cols == 5
+        assert d.n_rows == 4
+        assert d.n_cols == 5
 
     def test_pass_grid(self):
         grid = SquareGrid(3, 4)
         d = Dungeon(grid=grid)
-        assert d.grid.n_rows == 3
-        assert d.grid.n_cols == 4
+        assert d.n_rows == 3
+        assert d.n_cols == 4
