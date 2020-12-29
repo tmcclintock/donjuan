455bcab329ed8d5f3c7d143e0d083a886e5a78d9
James DeVore <jdevore4592@gmail.com>
2020-12-29 11:37:30 -0700

    Initial creation of DoorSpaceRandomizer

--- donjuan/randomizer.py
+++ donjuan/randomizer.py
@@ -1,7 +1,7 @@
 import random
 from typing import Optional
 
-from donjuan import Cell, Grid
+from donjuan import Cell, Grid, DoorSpace, Door,Portcullis,Archway
 
 
 class Randomizer:
@@ -16,6 +16,9 @@ class Randomizer:
     def randomize_grid(self, grid: Grid) -> None:
         """Randomize properties of the `Grid`"""
         pass  # pragma: no cover
+    def randomize_door(self, cell: DoorSpace):
+        """Randomize properties of the `DoorSpace`"""
+        pass #pragma: no cover
 
     @classmethod
     def seed(cls, seed: Optional[int] = None) -> None:
@@ -41,3 +44,30 @@ class RandomFilled(Randomizer):
             for j in range(grid.n_cols):
                 self.randomize_cell(grid.cells[i][j])
         return
+class DoorSpaceRandomizer(Randomizer):
+    """
+    Randomly set the :attrs: of the DoorSpace and select a DoorSpace type (Door, Archway, Portcullis).
+    """
+    def randomize_doorspace(self, door_space:DoorSpace):
+        #
+        door_types = [Door, Portcullis, Archway]
+        material_types = ["wood","metal","stone"]
+        #randomize all possible attributes
+        blocked = bool(random.randint(0,1))
+        secret = bool(random.randint(0,1))
+        locked = bool(random.randint(0,1))
+        broken = bool(random.randint(0,1))
+        jammed = bool(random.randint(0,1))
+        closed = bool(random.randint(0,1))
+        material = material_types[random.randint(0,len(material_types) - 1)]
+        #pick a type and make a new instance of it
+        selected_type = door_types[random.randint(0,len(door_types) - 1)]()
+
+        if(isinstance(selected_type, Door)):
+            door_space = Door(secret=secret,locked=locked,closed=closed,jammed=jammed,blocked=blocked,broken=broken,material=material)
+        elif(isinstance(selected_type, Portcullis)):
+            door_space = Portcullis(locked=locked,closed=closed,jammed=jammed,broken=broken,material=material)
+        elif(isinstance(selected_type, Archway)):
+            door_space = Archway(material=material,blocked=blocked,broken=broken,secret=secret)
+        return door_space
+        pass
