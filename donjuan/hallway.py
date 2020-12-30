06ad57b3b85e3771f55816b166a1ba5261c1e790
Tom McClintock <tmcclintock89@gmail.com>
2020-12-29 17:02:23 -0800

    Typo fix.

--- donjuan/hallway.py
+++ donjuan/hallway.py
@@ -3,7 +3,7 @@ from donjuan.space import Space
 
 class Hallway(Space):
     """
-    A hallway in a dungeon. It has a start an end cell.
+    A hallway in a dungeon. It has a start and end cell.
 
     .. todo:: Finish this class. It is incomplete.
     """
