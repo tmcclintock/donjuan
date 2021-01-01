8435b6b34986870813ddc9fad1f82fa30da5b159
Tom McClintock <tmcclintock89@gmail.com>
2020-12-31 16:26:26 -0800

    Added edge grid tests.

--- tests/grid_test.py
+++ tests/grid_test.py
@@ -19,6 +19,23 @@ class SquareGridTest(TestCase):
             for j in range(sg.n_cols):
                 assert sg.cells[i][j].coordinates == (i, j)
 
+    def test_edge_grid(self):
+        sg = SquareGrid(5, 4)
+        assert len(sg.edges) == 2
+        h, v = sg.edges
+        assert len(h) == sg.n_rows + 1
+        assert len(h[0]) == sg.n_cols
+        assert len(v) == sg.n_rows
+        assert len(v[0]) == sg.n_cols + 1
+        assert h[0][0].cell1 is None
+        assert h[0][0].cell2 is sg.cells[0][0]
+        assert v[0][0].cell1 is None
+        assert v[0][0].cell2 is sg.cells[0][0]
+        assert h[-1][-1].cell1 is sg.cells[-1][-1]
+        assert h[-1][-1].cell2 is None
+        assert v[-1][-1].cell1 is sg.cells[-1][-1]
+        assert v[-1][-1].cell2 is None
+
     def test_get_filled_grid(self):
         sg = SquareGrid(5, 4)
         fg = sg.get_filled_grid()
@@ -60,3 +77,21 @@ class HexGridTest(TestCase):
         assert hg is not None
         assert hg.n_rows == 5
         assert hg.n_cols == 4
+
+    def test_edge_grid(self):
+        gg = HexGrid(5, 4)
+        assert len(gg.edges) == 3
+        h = gg.edges[0]
+        assert len(h) == gg.n_rows + 1
+        assert len(h[0]) == gg.n_cols
+        for v in gg.edges[1:]:
+            assert len(v) == gg.n_rows
+            assert len(v[0]) == gg.n_cols + 1
+            assert v[0][0].cell1 is None
+            assert v[0][0].cell2 is gg.cells[0][0]
+            assert v[-1][-1].cell1 is gg.cells[-1][-1]
+            assert v[-1][-1].cell2 is None
+        assert h[0][0].cell1 is None
+        assert h[0][0].cell2 is gg.cells[0][0]
+        assert h[-1][-1].cell1 is gg.cells[-1][-1]
+        assert h[-1][-1].cell2 is None
