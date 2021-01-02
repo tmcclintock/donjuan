e5b5f6f7af979df0f2746e02a3b84cc3ea5b6b9a
Tom McClintock <tmcclintock89@gmail.com>
2021-01-02 09:54:41 -0800

    Smoke test for hallway.

--- tests/hallway_test.py
+++ tests/hallway_test.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
 
+from donjuan import Hallway
+
 
 class HallwayTest(TestCase):
     """
     .. todo:: Make tests once the `Hallway` class has content.
     """
 
-    pass
+    def test_smoke(self):
+        h = Hallway()
+        assert h is not None
