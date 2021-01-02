968318e99ef63bf31c3327c246c3da3f4998451f
Tom McClintock <tmcclintock89@gmail.com>
2021-01-02 11:03:25 -0800

    Building the Hallway class.

--- donjuan/hallway.py
+++ donjuan/hallway.py
@@ -1,3 +1,6 @@
+from typing import List, Optional, Set, Union
+
+from donjuan.cell import Cell
 from donjuan.space import Space
 
 
@@ -5,5 +8,23 @@ class Hallway(Space):
     """
     A hallway in a dungeon. It has a start and end cell.
 
-    .. todo:: Finish this class. It is incomplete.
+    .. todo:: Complete this class
     """
+
+    def __init__(
+        self, cells: Optional[Set[Cell]] = None, name: Union[int, str] = "",
+    ):
+        super().__init__(cells=cells, name=name)
+        self._cells_ordered: List[Cell] = []
+
+    @property
+    def cells_ordered(self) -> List[Cell]:
+        return self._cells_ordered
+
+    @property
+    def end_cell(self) -> Cell:
+        return self.cells_ordered[-1]
+
+    @property
+    def start_cell(self) -> Cell:
+        return self.cells_ordered[0]
