e76c56f038599c88656732f94d8b6ade7d5e2ed2
Tom McClintock <tmcclintock89@gmail.com>
2021-01-07 19:32:32 -0800

    Added attribute to the Edge.

--- donjuan/edge.py
+++ donjuan/edge.py
@@ -10,13 +10,16 @@ class Edge:
     Args:
         cell1 (Optional[Cell]): cell on one side of the edge
         cell2 (Optional[Cell]): cell on the other side of the edge
+        has_door (bool, optional): default ``False``, indicates whether this
+            object has a door
     """
 
     def __init__(
-        self, cell1: Optional[Cell] = None, cell2: Optional[Cell] = None,
+        self, cell1: Optional[Cell] = None, cell2: Optional[Cell] = None, has_door: bool = False,
     ):
         self._cell1 = cell1
         self._cell2 = cell2
+        self.has_door = has_door
 
     @property
     def cell1(self) -> Optional[Cell]:
@@ -35,7 +38,8 @@ class Edge:
     @property
     def is_wall(self) -> bool:
         return (
-            (self.cell1 is None)
+            (not self.has_door) 
+            or (self.cell1 is None)
             or (self.cell2 is None)
             or (self.cell1.filled != self.cell2.filled)
             or (self.cell1.space != self.cell2.space)
